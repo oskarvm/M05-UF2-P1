a7786014f3f9b272ab3221948ecb2243d8b78007
Oscar <ovazquez@elpuig.xeill.net>
2021-01-30 19:07:45 +0100

    Cambios branch

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Holaaa mon");
         System.out.println("Holaaa mon");
-
-
-        System.out.println("Adioos mon");
     }
 }
